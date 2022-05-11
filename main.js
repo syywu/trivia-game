1a50436a9843f5683fe49b86a88c0f39d18c0ed0
Samantha Wu <samanthayywu@gmail.com>
2022-05-11 19:54:04 +0100

    added json file

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 let easy = document.querySelector('#easy');
 let medium = document.querySelector('#medium');
 let hard = document.querySelector('#hard');
-let entertainment = document.querySelector('#entertainment');
-let generalKnowledge = document.querySelector('#general-knowledge');
-let vehicles = document.querySelector('#vehicles');
-let art = document.querySelector('#art');
-let geography = document.querySelector('#geography');
-let science = document.querySelector('#science');
-let celeberities = document.querySelector('#celeberities');
-let history = document.querySelector('#history');
-
 
 async function getData(){
-    let response = await fetch("https://opentdb.com/api.php?amount=1000");
+    let response = await fetch("https://opentdb.com/api.php?amount=50");
     let data = await response.json();
     console.log(data)
 
 }
+
+
 getData();
+
+// get questions
+function getQuestions(){
+
+}
+
+// get answers
+
+// start game 
+
+// end game
